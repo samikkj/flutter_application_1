50e0c14456dc81b4dfe80b4605ebb24d310484c9
samikkhan <77587298+samikkj@users.noreply.github.com>
2022-09-08 16:32:29 +0500

    Update home_page.dart

--- lib/Views/home_page.dart
+++ lib/Views/home_page.dart
@@ -14,80 +14,83 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Column(
-          children: [
-            CircleAvatar(),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: namTextController,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Name',
-                  hintText: 'Enter your name'),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: passTextController,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                  hintText: 'Enter your username'),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: conTextController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-                hintText: 'Enter your password',
-                suffixIcon: Icon(Icons.password, size: 20, color: Colors.amber),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              CircleAvatar(),
+              SizedBox(
+                height: 20,
               ),
-              obscureText: true,
-              // for passwords
-              obscuringCharacter: '*',
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: usrTextController,
-              decoration: InputDecoration(
+              TextField(
+                controller: namTextController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                    hintText: 'Enter your name'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: passTextController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Username',
+                    hintText: 'Enter your username'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: conTextController,
+                decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Confirm Password',
-                  hintText: 'Enter your password'),
-              obscureText: true,
-              // for passwords
-              obscuringCharacter: '*',
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => SecondScreen(
-                          name: namTextController.text,
-                          username: passTextController.text,
-                          password: conTextController.text,
-                          confirmpassword: usrTextController.text,
-                        )));
-              },
-              child: const Text('Login'),
-              style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all(Colors.amber),
-                  backgroundColor: MaterialStateProperty.all(
-                    Colors.blue,
-                  ),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)))),
-            ),
-          ],
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  suffixIcon:
+                      Icon(Icons.password, size: 20, color: Colors.amber),
+                ),
+                obscureText: true,
+                // for passwords
+                obscuringCharacter: '*',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: usrTextController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Confirm Password',
+                    hintText: 'Enter your password'),
+                obscureText: true,
+                // for passwords
+                obscuringCharacter: '*',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => SecondScreen(
+                            name: namTextController.text,
+                            username: passTextController.text,
+                            password: conTextController.text,
+                            confirmpassword: usrTextController.text,
+                          )));
+                },
+                child: const Text('Login'),
+                style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all(Colors.amber),
+                    backgroundColor: MaterialStateProperty.all(
+                      Colors.blue,
+                    ),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)))),
+              ),
+            ],
+          ),
         ));
   }
 }
